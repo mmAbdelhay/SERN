c09d14d6246a9a13473b964deae0ca1f5ce9fcea
abdelhay <abdelhay.courses@gmail.com>
2021-10-18 20:58:31 +0200

    apply auth to admin dashboard

--- admin/adminPanel.js
+++ admin/adminPanel.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const AdminBro = require("admin-bro");
 const AdminBroExpress = require("@admin-bro/express");
 const AdminBroSequelize = require("@admin-bro/sequelize");
@@ -8,8 +10,28 @@ module.exports = function (app) {
   const adminBro = new AdminBro({
     databases: [db],
     rootPath: "/admin",
+    logoutPath: "/admin/exit",
+    loginPath: "/admin/login",
+    branding: {
+      companyName: "Abdelhay",
+    },
   });
 
-  const router = AdminBroExpress.buildRouter(adminBro);
+  const ADMIN = {
+    email: process.env.ADMIN_EMAIL,
+    password: process.env.ADMIN_PASSWORD,
+  };
+
+  const router = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
+    authenticate: async (email, password) => {
+      if (ADMIN.password === password && ADMIN.email === email) {
+        return ADMIN;
+      }
+      return null;
+    },
+    cookieName: "adminbro",
+    cookiePassword: process.env.ADMIN_COOKIE_PASSWORD,
+  });
+
   app.use(adminBro.options.rootPath, router);
 };
